7dacaeefa9fc37b0757e1f0b056640642cdfd9b5
nilhansuer <75779663+nilhansuer@users.noreply.github.com>
2022-08-01 18:27:55 +0300

    Add project files.

---
+++ AdaApplication/AdaApplication/MainPage2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdaApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClicked_User(object o, EventArgs args)
+        {
+            if (userName.Text == "admin" && userPassword.Text == "123")
+            {
+                await Navigation.PushModalAsync(new HomePage());
+            }
+            else
+            {
+                Task task = DisplayAlert("OOPS!", "Your username or password is wrong!", "OK");
+            }
+        }
+
+        async void OnClicked_NewUser(object o, EventArgs args)
+        {
+
+            await Navigation.PushModalAsync(new NewUserPage());
+        }
+    }
+}
---
+++ AdaApplication/AdaApplication/NewUserPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AdaApplication
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewUserPage : ContentPage
+    {
+        public NewUserPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClicked_Confirm(object o, EventArgs args)
+        {
+            // userName = new_username ?????
+
+            await Navigation.PushModalAsync(new MainPage2());
+
+        }
+    }
+}
